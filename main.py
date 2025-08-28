ec8001d7b3159569190f6e525fd56b79b7e1e36b
shay <shay.manor@gmail.com>
2025-08-28 15:17:21 -0400

    chore: fixed flask not loading

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder="static", static_url_path="")
+app = Flask(__name__, static_folder="src", static_url_path="")
 
 
 @app.route("/", methods=["GET"])
