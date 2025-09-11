c2268e750f7edb84e4d43db2dd92b8789c3326fb
shay <shay.manor@gmail.com>
2025-09-11 11:41:41 -0400

    feat: added blog post generator to flask server. Connected to supabase.

--- main.py
+++ main.py
@@ -1,8 +1,53 @@
+import json
 import os
+import uuid
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional
 
-from flask import Flask, send_from_directory
+import requests
+from flask import Flask, send_from_directory, request, jsonify
+from flask_cors import CORS
+from openai import OpenAI
+from supabase import create_client, Client
 
 app = Flask(__name__, static_folder="src", static_url_path="")
+CORS(app)
+
+def get_body_system(date): return f"You are an expert at providing a real, accurate summary of the major news from the last day. You have access to web searches and hackernews. Summarize the top hackernews articles and **IMPORTANT** provide a realistic analysis and synthesis of these. The current date is: {date}. Only return the overall summary as an article form and use critical thinking to analyze each. You will be given the top posts from hackernews and access to a web search tool and will use this to check the **MOST** interesting, relevant posts to talk about. Some interesting post example titles could be 'Using Claude Code to modernize a 25-year-old kernel driver', 'Myanmar – Silk Road of Surveillance', 'Stripe Launches L1 Blockchain: Tempo', 'LLM Visualization', or other interesting articles involving ML, math, cybersecurity, or crypto. Ignoring posts is fine. Focus on posts that talk about new technologies, AI/ML breakthroughts, or important relevant topics. Be concise, this should be a 2-5 minute read. Don't put a title, just the body in markdown. Only return the body, nothing else. Read the real articles, don't just use the titles. Go through the important links and summarize them."
+
+def get_title_system(): return "You are an expert at taking an article and giving it a short, clear title. No need for super dramatic titles. It should be 2-10 words and accurately encapsulate what happens and the overall theme. Only return the title."
+
+def get_tldr_system(): return "You are an expert at summarizing technical articles in a clear and understandable way. Without losing important information, write a 1 paragraph, short and clear tldr for this article. Be accurate. Only return the paragraph tldr summary."
+
+@dataclass
+class Article:
+    by: str
+    descendants: int
+    id: int
+    score: int
+    time: int
+    title: str
+    type: str
+    url: Optional[str]
+    text: Optional[str]
+
+
+def get_articles() -> str:
+    articles = []
+    ids_list = requests.get("https://hacker-news.firebaseio.com/v0/topstories.json").text[1:-1].split(',')[:20]
+    for id in ids_list:
+        raw_data = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{id}.json").text
+        data = json.loads(raw_data)
+        if data["type"] != "story":
+            continue
+        art = Article(data["by"], int(data["descendants"]), int(data["id"]), int(data["score"]), int(data["time"]), data["title"], data["type"], data.get("url", ""), data.get("text", ""))
+        articles.append(art)
+    sorted_art = sorted(articles, key=lambda a: a.score, reverse=True)
+    res = "{\n"
+    for r in sorted_art:
+        res += f"{r.title}:{r.url},\n"
+    return res + "}"
 
 
 @app.route("/", methods=["GET"])
@@ -13,6 +58,45 @@ def root():
 @app.route("/<page>", methods=["GET"])
 def any_page(page):
     return send_from_directory(app.static_folder, f"{page}")
+
+
+@app.route("/create_blog_post", methods=["POST"])
+def create_blog():
+    data = request.get_json()
+    SUPABASE_SERVICE_KEY = data["SUPABASE_SERVICE_KEY"]
+    SUPABASE_URL = data["SUPABASE_URL"]
+    OPENAI_API_KEY = data["OPENAI_API_KEY"]
+
+    # Located here to block anyone from pinging this with spoofed keys
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
+
+    def ping_gpt(system, prompt, effort) -> str:
+        client = OpenAI(api_key=OPENAI_API_KEY)
+        response = client.responses.create(
+            model="gpt-5",
+            reasoning={"effort": effort},
+            input=prompt,
+            instructions=system,
+            tools=[{
+                    "type": "web_search"
+                   }]
+        )
+        return response.output_text
+    articles = get_articles()
+    current_date = date.today()
+    body = ping_gpt(system=get_body_system(current_date.strftime("%B %d")), prompt=str(articles), effort="high")
+    title = ping_gpt(system=get_title_system(), prompt=body, effort="low")
+    tldr = ping_gpt(system=get_tldr_system(), prompt=body, effort="medium")
+
+
+    data_to_insert = {"id": str(uuid.uuid4()), "title": title, "body": body, "tldr": tldr}
+    response = (
+        supabase.table("site_blog")
+        .insert(data_to_insert)
+        .execute()
+    )
+    print(response)
+    return jsonify({"status": response.data["id"]})
 
 
 if __name__ == "__main__":
